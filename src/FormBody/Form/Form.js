5b17821da072d5d5923991cbfa7d62a4e0094dbd
techamit95ch <amit98ch@gmail.com>
2021-07-23 19:27:28 +0530

    Done

--- src/FormBody/Form/Form.js
+++ src/FormBody/Form/Form.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity, Button } from "react-native";
 import { Card, ListItem, Icon, Switch } from "react-native-elements";
-
+import { AntDesign } from '@expo/vector-icons';
 const Form = () => {
   return (
     <View style={styles.form}>
@@ -24,13 +24,13 @@ const Form = () => {
               alignItems: "flex-end",
             }}
           >
-            <Switch color="green" />
+            <Switch color="green" value={true} />
           </View>
         </TouchableOpacity>
         <Card.Divider />
 
-        <View containerStyle={{ padding: 0 }}>
-          <View style={{ paddingLeft: 30 }}>
+        <View style={{ paddingHorizontal: 30 }}>
+          <View >
             <TouchableOpacity style={styles.coldevide}>
               <View style={{ flex: 2 }}>
                 <Card.Title
@@ -83,7 +83,7 @@ const Form = () => {
                   alignItems: "flex-end",
                 }}
               >
-                <Switch value={false} color="green" />
+                <Switch value={true} color="green" />
               </View>
             </TouchableOpacity>
 
@@ -111,7 +111,7 @@ const Form = () => {
                   alignItems: "flex-end",
                 }}
               >
-                <Switch value={false} color="green" />
+                <Switch value={true} color="green" />
               </View>
             </TouchableOpacity>
 
@@ -139,7 +139,7 @@ const Form = () => {
                   alignItems: "flex-end",
                 }}
               >
-                <Switch value={false} color="green" />
+                <Switch value={true} color="green" />
               </View>
             </TouchableOpacity>
 
@@ -167,7 +167,7 @@ const Form = () => {
                   alignItems: "flex-end",
                 }}
               >
-                <Switch value={false} color="green" />
+                <Switch value={true} color="green" />
               </View>
             </TouchableOpacity>
 
@@ -241,8 +241,45 @@ const Form = () => {
                 <Switch value={false} color="green" />
               </View>
             </TouchableOpacity>
+            <TouchableOpacity style={styles.coldevide}>
+              <View style={{ flex: 2 }}>
+                <Card.Title
+                  style={{
+                    fontSize: 12,
+                    fontWeight: "bold",
+                    textAlign: "left",
+                  }}
+                >
+                  Clear Questions
+                </Card.Title>
+                <Text
+                  style={{ textAlign: "left", fontSize: 10, color: "grey" }}
+                >
+                  kjshksdh
+                </Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: "center",
+                  alignItems: "flex-end",
+                }}
+              >
+                <Switch value={true} color="green" />
+              </View>
+            </TouchableOpacity>
           </View>
         </View>
+      </View>
+      <View style={styles.card2}>
+
+                <View></View>
+                <Button title="Save" style={styles.icon} size={20} color={"#6530a2"}/>
+      </View>
+      <View style={{marginTop: 10,flex:1}}>
+
+                <AntDesign name="closecircleo" size={24} color="grey" />
+                
       </View>
     </View>
   );
@@ -255,16 +292,29 @@ const styles = StyleSheet.create({
     display: "flex",
     flex: 4,
     height: 500,
-    borderRadius: 10,
-    justifyContent: "center",
+    borderRadius: 9,
+    flexDirection: "row",
+    // flexWrap: "wrap",
+    // alignItems: "flex-start",
+    justifyContent: "space-between",
     "box-shadow": "0px 0px 10px 0px rgba(0,0,0,0.57)",
     "-webkit-box-shadow": "0px 0px 10px 0px rgba(0,0,0,0.57)",
     "-moz-box-shadow": "0px 0px 10px 0px rgba(0,0,0,0.57)",
   },
   card: {
     display: "flex",
+    flex:10,
     textAlign: "left",
     paddingHorizontal: 30,
+    justifyContent: "center",
+  },
+  card2: {
+    display: "flex",
+    flex:2,
+    textAlign: "left",
+    paddingHorizontal: 30,
+    justifyContent: "space-between",
+    marginVertical: 20
   },
   coldevide: {
     display: "flex",
